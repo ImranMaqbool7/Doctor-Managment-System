e7abed43573d4a5305f0e159f8b9c97e6c01a8b6
imran-fullstack <imranmaqbool563@gmail.com>
2026-07-24 10:18:21 +0500

    frontend

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
+  "https://imran-maqbool-arvr.vercel.app/",
 ];
 
 // ======================
