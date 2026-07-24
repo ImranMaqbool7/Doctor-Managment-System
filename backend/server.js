b770369dae1c26e8ec91d4b784ac33471c327aef
imran-fullstack <imranmaqbool563@gmail.com>
2026-07-24 17:53:55 +0500

    fgg

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://doctor-managment-system-o8ev.vercel.app",
+  "https://doctor-managment-system-sazn.vercel.app/",
 ];
 
 // ======================
